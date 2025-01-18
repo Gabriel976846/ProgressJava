af2be312cbcc0ada1257c70e8ff7c32199ff81de
Gabriel976846 <sergiogabriel_santos18@hotmail.com>
2025-01-17 21:55:46 -0300

    Atualização - 17/01//2025
    Atualização do exercício, utilizando novos conhecimento

--- CodigosFeitosDuranteEstudos/MediaNota/Main.java
+++ CodigosFeitosDuranteEstudos/MediaNota/Main.java
@@ -9,10 +9,36 @@ public class Main {
         Scanner EntradaInformacoes = new Scanner(System.in);
 
         // Objetos Utilizados
-        Teste nota1 = new Teste();
+        Teste Final = new Teste();
+        Teste valor1 = new Teste();
+        Teste valor2 = new Teste();
 
+        // Adiquirindo nota 1
         System.out.print("Nota 1: ");
-        nota1.PrimeiraNota = EntradaInformacoes.nextInt();
-        System.out.println(nota1);
+        valor1.PrimeiraNota = EntradaInformacoes.nextDouble();
+        double Nota1 = valor1.PrimeiraNota;
+
+        // Adiquirindo nota 2
+        System.out.print("Nota 2: ");
+        valor2.SegundaNota = EntradaInformacoes.nextDouble();
+        double Nota2 = valor2.SegundaNota;
+
+        if (Nota1 >= 0 && Nota1 <= 10) {
+            if (Nota2 >= 0 && Nota2 <= 10){
+
+                double Mediafinal = Final.Media(Nota1, Nota2);
+                double MediaFInal = Final.resultado(Mediafinal);
+
+                if (Mediafinal >= 7) {
+                    Final.Aprovado();
+                } else if (Mediafinal < 7 && Mediafinal >= 4) {
+                    Final.AvaliacaoFinal();
+                    System.out.println("Precisa de: " + MediaFInal);
+                } else {
+                    Final.Reprovado();
+                }
+            }
+        }
+        EntradaInformacoes.close();
     }
 }
--- CodigosFeitosDuranteEstudos/MediaNota/Teste.java
+++ CodigosFeitosDuranteEstudos/MediaNota/Teste.java
@@ -1,10 +1,26 @@
 package CodigosFeitosDuranteEstudos.MediaNota;
 
 public class Teste {
-    int PrimeiraNota;
-    int SegundaNota;
+    double PrimeiraNota;
+    double SegundaNota;
 
     public void Aprovado(){
         System.out.println("Você foi aprovado");
+    }
+
+    public void AvaliacaoFinal(){
+        System.out.println("Você vai para as provas Finais");
+    }
+
+    public void Reprovado(){
+        System.out.println("Você foi reprovado");
+    }
+
+    public double Media(double x, double y){
+        return (x + y) / 2;
+    }
+
+    public double resultado(double Resultado){
+        return 10 - Resultado;
     }
 }
